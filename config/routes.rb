e3410185f10d5237e3d28d2b88b2c5ea5b01f91c
Steve Fenn <steve@bearwho.codes>
2018-04-30 15:03:29 +0100

    Admin article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :articles
+  end
   resources :projects, only: [:index, :show]
   resources :articles, only: [:index, :show]
   resources :workshop, only: [:index, :show]
   get 'about-me', to: 'pages#about'
   root 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
