4140362185b760f8b1c0873be3655c9e3eedb2a4
Steve Fenn <steve@bearwho.codes>
2018-04-30 13:46:18 +0100

    Initial projects files

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,13 @@
+class ProjectsController < ApplicationController
+
+  # GET /projects
+  def index
+    @projects = Project.all
+  end
+
+  # GET /projects/1
+  def show
+    @project = Project.find(params[:id])
+  end
+
+end
