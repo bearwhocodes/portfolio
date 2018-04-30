7dd05fa74203a0fda80f713bc8c24fd06941f83c
Steve Fenn <steve@bearwho.codes>
2018-04-30 14:13:03 +0100

    Initial pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+
+  # GET /
+  # GET /pages/home
+  def home
+  end
+
+  # GET /about-me
+  # GET /pages/about
+  def about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :show]
   resources :articles, only: [:index, :show]
   resources :workshop, only: [:index, :show]
+  get 'about-me', to: 'pages#about'
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
