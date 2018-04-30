2635b6cfe967e7f213aec6bc1406e65507d4cc3e
Steve Fenn <steve@bearwho.codes>
2018-04-30 13:44:31 +0100

    Initial articles files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+
+  # GET /articles
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  def show
+    @article = Article.find(params[:id])
+  end
+
+end
