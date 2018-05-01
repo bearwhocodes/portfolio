72a863384ffccfb5908654d85cc989587d632b84
Steve Fenn <steve@bearwho.codes>
2018-05-01 12:04:38 +0100

    Adding pen model

--- app/models/pen.rb
+++ app/models/pen.rb
@@ -1,2 +1,17 @@
 class Pen < ApplicationRecord
+
+  # Uploaders
+  mount_uploader :image, PenImageUploader
+
+  # Validations
+  validates :name, :summary, :codepen_url, presence: true
+
+  # Scopes
+  scope :unarchived, -> { where(archived: false) }
+
+  # Methods
+  def to_s
+    name
+  end
+
 end
--- db/migrate/20180430125011_create_pens.rb
+++ db/migrate/20180430125011_create_pens.rb
@@ -1,7 +1,11 @@
 class CreatePens < ActiveRecord::Migration[5.1]
   def change
     create_table :pens do |t|
-
+      t.string :name
+      t.text :summary
+      t.string :codepen_url
+      t.string :image
+      t.boolean :archived
       t.timestamps
     end
   end
