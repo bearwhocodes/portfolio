df16e44bcb31598f9007c6afc08bae0f9ebc80e8
Steve Fenn <steve@bearwho.codes>
2018-05-01 11:15:04 +0100

    Project Model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,20 @@
 class Project < ApplicationRecord
+
+  # Relationships
+  has_and_belongs_to_many :technologies
+
+  # Uploaders
+  mount_uploader :image, ProjectImageUploader
+
+  # Validations
+  validates :name, :summary, :content, presence: true
+
+  # Scopes
+  scope :unarchived, -> { where(archived: false) }
+
+  # Methods
+  def to_s
+    name
+  end
+  
 end
--- db/migrate/20180430124440_create_projects.rb
+++ db/migrate/20180430124440_create_projects.rb
@@ -1,7 +1,13 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
-
+      t.string :name
+      t.text :summary
+      t.string :github_url
+      t.string :website_url
+      t.text :content
+      t.string :image
+      t.boolean :archived
       t.timestamps
     end
   end
