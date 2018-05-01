66c077ded7faea102e5651ff43561ea7da2f45cb
Steve Fenn <steve@bearwho.codes>
2018-05-01 10:58:11 +0100

    Adding articles model and migrations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,31 @@
 class Article < ApplicationRecord
+
+  # Acts as
+  acts_as_taggable
+
+  # Callbacks
+  before_validation :set_reading_time
+
+  # Uploaders
+  mount_uploader :image, ArticleImageUploader
+
+  # Validations
+  validates :title, :summary, :reading_time, :content, :published_at, presence: true
+
+  # Scopes
+  scope :unarchived, -> { where(archived: false) }
+  scope :published, -> { where("published_at <= ?", Time.now)}
+
+  # Methods
+  def to_s
+    title
+  end
+
+private
+
+  def set_reading_time
+    content = ActionView::Base.full_sanitizer.sanitize(self.content)
+    self.reading_time = content.reading_time format: :approx
+  end
+  
 end
--- db/migrate/20180430124109_create_articles.rb
+++ db/migrate/20180430124109_create_articles.rb
@@ -1,7 +1,13 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
-
+      t.string :title
+      t.text :summary
+      t.text :content
+      t.string :reading_time
+      t.string :image
+      t.datetime :published_at
+      t.boolean :archived
       t.timestamps
     end
   end
